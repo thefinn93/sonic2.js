edec55d22edf6707d74cfd4f89b27a2167724516
Finn <finn@finn.io>
2016-01-14 16:12:05 -0800

    unbroke everything, it actually does the basics now. omg

--- sonic2.js
+++ sonic2.js
@@ -1,64 +1,116 @@
 var http = require('http');
+var util = require('util');
+var EventEmitter = require('events');
 var url = require('url');
 var Q = require('q');
 
-function Mifi(options) {
-  options = options || {};
-  this.requestBase = url.parse(options.url || "http://192.168.0.1/soda");
-}
-
-Mifi.prototype.makeRawRequest = function(postdata) {
-  var deferred = Q.defer();
-  var options = this.requestBase;
-
-  options.headers = {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Content-Length': postdata.length
+function Mifi() {
+  var options = {
+    url: "http://192.168.0.1/soda",
+    events: {
+      battery: "standard",
+      dm: "standard",
+      hotspot: "basic",
+      settings: "basic",
+      sms: "standard",
+      wwan: "basic"
+    }
   };
 
-  options.method = 'POST';
-  var req = http.request(options, function(res) {
-    res.setEncoding('utf8');
-    var response = "";
-    res.on('data', function(chunk) {
-      response += chunk;
+  var self = this;
+  var clientid = 0;
+  var requestBase = url.parse(options.url);
+
+  EventEmitter.call(this);
+
+  function makeRawRequest(postdata) {
+    var deferred = Q.defer();
+    var options = requestBase;
+
+    options.headers = {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': postdata.length
+    };
+
+    options.method = 'POST';
+    var req = http.request(options, function(res) {
+      res.setEncoding('utf8');
+      var response = "";
+      res.on('data', function(chunk) {
+        response += chunk;
+      });
+      res.on('end', function() {
+        try {
+          deferred.resolve(JSON.parse(response));
+        } catch(e) {
+          deferred.reject({
+            error: "Failed to parse response as JSON",
+            "response": response
+          });
+        }
+      });
     });
-    res.on('end', function() {
-      try {
-        deferred.resolve(JSON.parse(response));
-      } catch(e) {
-        deferred.reject({
-          error: "Failed to parse response as JSON",
-          "response": response
-        });
-      }
+
+    req.on('error', function(e) {
+      deferred.reject(e);
     });
-  });
 
-  req.on('error', function(e) {
-    deferred.reject(e);
-  });
+    req.write(postdata);
+    req.end();
 
-  req.write(postdata);
-  req.end();
+    return deferred.promise;
+  }
 
-  return deferred.promise;
-};
-
-Mifi.prototype.makeSodaCall = function(uri, action, params) {
-  var postdata = JSON.stringify({
-    soda: {
+  function makeSodaCall(uri, args) {
+    var sodamsg = {
       'message-id': 0,
-      uri: uri,
-      action: action,
-      params: params
+      uri: uri
+    };
+    for(var arg in args) {
+      if(args.hasOwnProperty(arg)) {
+        sodamsg[arg] = args[arg];
+      }
     }
-  });
-  return this.makeRawRequest(postdata);
-};
+    var postdata = JSON.stringify({soda: sodamsg});
+    return makeRawRequest(postdata);
+  }
 
-Mifi.prototype.send_sms = function(to, message) {
-    return this.makeSodaCall('/soda/sms/message', 'create', {to: to, content: message});
-};
+  function processPoll(result) {
+    if(result.soda) {
+      if(result.soda['client-id'] !== undefined && result.soda['client-id'] != clientid) {
+        // Our client ID has been changed (or, most likely assigned)
+        clientid = result.soda['client-id'];
+      }
+
+      for(var eventType in options.events) {
+        if(result.soda.hasOwnProperty(eventType)) {
+          self.emit(eventType, result.soda[eventType]);
+        }
+      }
+
+      // Shows up after the initial request
+      if(result.soda['long-polling']) {
+        var params = {
+          'client-id': clientid,
+          params: options.events
+        };
+        self.emit('connected', clientid);
+        return makeSodaCall('/soda/notify', params).then(processPoll);
+      } else {
+        return self.poll();
+      }
+    }
+  }
+
+  this.send_sms = function send_sms(to, message) {
+      return makeSodaCall('/soda/sms/message', {action: 'create', params: {to: to, content: message}});
+  };
+
+  this.poll = function poll() {
+    return makeSodaCall('/soda/notify', {'client-id': clientid}).then(processPoll);
+  };
+}
+
+util.inherits(Mifi, EventEmitter);
 
 module.exports = Mifi;
--- test.js
+++ test.js
@@ -1,9 +1,32 @@
 #!/usr/bin/env node
+process.on('uncaughtException', function (err) {
+    console.log(err.stack || err);
+});
+
 var Mifi = require('./sonic2.js');
 
 var mifi = new Mifi();
-mifi.send_sms(process.env.TEST_NUMBER, 'testing').then(function(result) {
-  console.dir(result);
-}).catch(function(e) {
-  console.log(e.stack || e);
-});
+if(process.env.TEST_NUMBER) {
+  console.log('Sending message to', TEST_NUMBER);
+  mifi.send_sms(process.env.TEST_NUMBER, 'testing').then(function(result) {
+    console.dir(result);
+  }).catch(function(e) {
+    console.log(e.stack || e);
+  });
+}
+
+
+
+function log(e) {
+  console.dir(e, {depth: 100});
+}
+
+mifi.on('connect', log);
+mifi.on('battery', log);
+mifi.on('dm', log);
+mifi.on('hotspot', log);
+mifi.on('settings', log);
+mifi.on('sms', log);
+mifi.on('wwan', log);
+
+mifi.poll();
